8c3c9b82250fa85bc8c4a62be23f92e6e1a297b6
Thinh Vo <ducthinh2410@gmail.com>
2020-04-19 08:45:34 +0300

    Use async/await

--- sw.js
+++ sw.js
@@ -2,19 +2,19 @@ var cacheName = 'hello-pwa';
 var filesToCache = ['/', '/index.html', '/css/style.css', '/js/main.js'];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
+self.addEventListener('install', (e) => {
+  const preCache = async () => {
+    const cache = await caches.open(cacheName);
+    return cache.addAll(filesToCache);
+  };
+  e.waitUntil(preCache());
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(async function () {
+    const cachedResponse = await caches.match(e.request);
+    if (cachedResponse) return cachedResponse;
+    return fetch(e.request);
+  });
 });
